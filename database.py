e03622489ddae4aa946ab4958edbb5fddd4f84a9
bufonen <santigon8787@gmail.com>
2025-08-21 20:23:19 -0500

    Prime commit en dev

---
+++ database.py
@@ -0,0 +1,13 @@
+import pyodbc
+
+server = 'SANTIAGO\\SQLEXPRESS' #nombre del servidor sql
+database = 'MIBASE' #nombre de la base de datos
+connection_string = (
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    f'SERVER={server};'
+    f'DATABASE={database};'
+    'Trusted_Connection=yes;'
+)
+
+def get_connection():
+    return pyodbc.connect(connection_string)
